905763ccaa40e1b586d43e871c46d409883f51b6
OsvanirSilva <133386009+OsvanirSilva@users.noreply.github.com>
2024-05-29 10:11:28 -0300

    alteração

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Tarefas',
+      title: 'App',
       initialRoute: 'login',
       routes: {
         'login': (context) => LoginView(),
